14113ac16556570d56055ca4f9c178bb85feeb90
Sachith Dilshan <sachithdilshan404@gmail.com>
2023-08-30 12:57:15 +0530

    fix bugs

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout( { children } : { children: React.ReactNode }
         <ClerkProvider>
             <html lang="en">
                 <body className={`${inter.className} bg-dark-1`}>
-                    <div className="w-full flex justify-center items-center">
+                    <div className="w-full flex justify-center items-center min-h-screen">
                     {children}
                     </div>
                 </body>
