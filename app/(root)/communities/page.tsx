7e358c38807ec5c74b71d65b91549e357a8f361a
Sachith Dilshan <sachithdilshan404@gmail.com>
2023-08-30 12:42:54 +0530

    first commit

---
+++ app/(root)/communities/page.tsx
@@ -0,0 +1,7 @@
+const Page = async () => {
+    return (
+        <section className="head-text mb-10">Communities</section>
+    )
+}
+
+export default Page;
---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,54 @@
+
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import UserCard from "@/components/cards/UserCard";
+
+async function Page() {
+
+    const user = await currentUser();
+
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+
+    //Fetch users
+    const result = await fetchUsers({
+        userId: user.id,
+        searchString: '',
+        pageNumber: 1,
+        pageSize: 25
+    })
+    if(!userInfo?.onboarded) redirect('/onboarding');
+    return (
+        <section className="head-text mb-10">Search
+        {/*Search bar*/}
+        <div className="mt-14 flex flex-col gap-9">
+            {result.users.length === 0 ? (
+                <p className="no-result">No Users</p>
+            ): (
+                <>
+                {result.users.map((person) => (
+                    <UserCard
+                    key={person.id}
+                    id={person.id}
+                    name={person.name}
+                    username={person.username}
+                    imgUrl={person.image}
+                    personType='User'
+                    />
+                ))}
+                </>
+            )}
+        </div>
+        </section>
+
+        
+    )
+}
+
+export default Page;
